042c088c36250b9d4236da5f1354c5be14a2bc30
flymargin <aiborxuesheng@163.com>
2017-01-06 21:17:10 +0800

    Add files via upload
    Win32_lib/giflib/include

---
+++ Win32_lib/giflib/include/stdint.h
@@ -0,0 +1,16 @@
+#ifndef __STDINT_H__
+#define __STDINT_H__
+
+/* Signed.  */
+typedef signed char		int8_t;
+typedef short int		int16_t;
+typedef int			int32_t;
+typedef long int		int64_t;
+
+/* Unsigned.  */
+typedef unsigned char		uint8_t;
+typedef unsigned short int	uint16_t;
+typedef unsigned int		uint32_t;
+typedef unsigned long int	uint64_t;
+
+#endif //__STDINT_H__
